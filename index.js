d8374c95d0cb72d609c03705ff39de4f77d63810
Usama Masood <umasood.bee17seecs@seecs.edu.pk>
2023-01-01 18:53:58 +0500

    Addition of 'see more' section for mobile menu

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const hamburger = document.getElementById('hamburger-btn');
 const mobMenu = document.getElementById('mobile_menu');
-
 function myFunction(x) {
   x.classList.toggle('change');
   if (mobMenu.style.visibility === 'visible') {
@@ -13,7 +12,7 @@ function myFunction(x) {
     mobMenu.style.overflowY = 'auto';
   }
 }
-
+hamburger.addEventListener('click', myFunction);
 const professors = [
   {
     name: 'Yann LeCun',
@@ -52,37 +51,99 @@ const professors = [
     img: 'images/Chon-Kilnam.jpg',
   },
 ];
-
 const referencePosition = document.getElementById('main-program');
-
 const createProfessorStructureHTML = () => {
   referencePosition.insertAdjacentHTML('afterend',
     `<section class="professors" id="featured-professors">
      <div class="section-title"><h2>Featured Professors</h2></div>
      <span class="red-bar"></span>
-     <ul class="professors-container"></ul></section>`);
+     <ul id="professors-list" class="professors-container"></ul></section>`);
 };
-
+let count = 0;
 const createProfessorCard = () => {
-  professors.forEach((professor) => {
+  for (let i = 0; i < 2; i += 1) {
     const professorsGrid = document.querySelector('.professors-container');
-
     professorsGrid.insertAdjacentHTML('beforeend',
       `<li class="card">
          <div class="professor__image">
-         <img src="${professor.img}" alt="professor photo"></div>
-        <div class="professor__dsc">
-        <h3>${professor.name}</h3>
-        <p class="dsc-1">${professor.profession}</p>
-        <span></span>
-        <p class="dsc-2">${professor.description}</p>
-        </div>
-        </li>`);
-  });
+         <img src="${professors[i].img}" alt="professor photo"></div>
+         <div class="professor__dsc">
+         <h3>${professors[i].name}</h3>
+         <p class="dsc-1">${professors[i].profession}</p>
+         <span></span>
+         <p class="dsc-2">${professors[i].description}</p>
+         </div>
+         </li>`);
+  }
+  const prfessorsGrid = document.querySelector('.professors-container');
+  prfessorsGrid.insertAdjacentHTML('afterend',
+    '<div id="see-more" class="se-more" onclick="mFunction()">More</div>');
 };
-
-document.addEventListener('DOMContentLoaded', () => {
-  createProfessorStructureHTML();
-
-  createProfessorCard();
-});
+function myUnction(x) {
+  if (document.getElementById('professors-list').getElementsByClassName('card').length > 0) {
+    while (document.getElementById('professors-list').getElementsByClassName('card').length > 0) {
+      document.getElementById('professors-list').removeChild(document.getElementById('professors-list').lastChild);
+    }
+  }
+  if (x.matches) {
+    professors.forEach((professor) => {
+      const professorsGrid = document.querySelector('.professors-container');
+      professorsGrid.insertAdjacentHTML('beforeend',
+        `<li class="card">
+           <div class="professor__image">
+           <img src="${professor.img}" alt="professor photo"></div>
+          <div class="professor__dsc">
+          <h3>${professor.name}</h3>
+          <p class="dsc-1">${professor.profession}</p>
+          <span></span>
+          <p class="dsc-2">${professor.description}</p>
+          </div>
+          </li>`);
+    });
+    if (count > 0) {
+      const elemt = document.getElementById('see-more');
+      elemt.remove();
+    }
+    count += 1;
+  } else {
+    count += 1;
+    if (document.getElementById('professors-list').getElementsByClassName('card').length > 0) {
+      while (document.getElementById('professors-list').getElementsByClassName('card').length > 0) {
+        document.getElementById('professors-list').removeChild(document.getElementById('professors-list').lastChild);
+      }
+    }
+    createProfessorCard();
+  }
+}
+createProfessorStructureHTML();
+const screenSize = window.matchMedia('(min-width: 768px)');
+myUnction(screenSize);
+screenSize.addListener(myUnction);
+function mFunction() {
+  if (document.getElementById('professors-list').getElementsByClassName('card').length < 3) {
+    for (let i = 2; i < 6; i += 1) {
+      const professorsGrid = document.querySelector('.professors-container');
+      professorsGrid.insertAdjacentHTML('beforeend',
+        `<li class="card">
+         <div class="professor__image">
+          <img src="${professors[i].img}" alt="professor photo"></div>
+          <div class="professor__dsc">
+          <h3>${professors[i].name}</h3>
+          <p class="dsc-1">${professors[i].profession}</p>
+          <span></span>
+          <p class="dsc-2">${professors[i].description}</p>
+          </div>
+          </li>`);
+    }
+    document.getElementById('see-more').innerHTML = 'Less';
+  } else {
+    while (document.getElementById('professors-list').getElementsByClassName('card').length > 2) {
+      document.getElementById('professors-list').removeChild(document.getElementById('professors-list').lastChild);
+    }
+    document.getElementById('see-more').innerHTML = 'More';
+  }
+}
+const val = 0;
+if (val === 1) {
+  mFunction();
+}
